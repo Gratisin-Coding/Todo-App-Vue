b7e822cd331d5ec5886631e714d69594a91c2476
Erick Christian <erickchristian48@gmail.com>
2021-07-21 08:26:50 +0700

    feat: u know this is just a todo app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Todo List</h1>
+
+  <form @submit.prevent="onSubmit">
+    <input type="text" placeholder="whatcha gonna do" v-model="inputValue" />
+    <button type="submit">Add Todo</button>
+  </form>
+
+  <ol>
+    <TodoItem
+      v-for="todo in todos"
+      :key="todo"
+      :onDelete="() => deleteTodo(todo)"
+      :todo="todo"
+    />
+  </ol>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import TodoItem from './components/TodoItem.vue'
+
+type DataTypes = {
+  todos: string[]
+  inputValue: string
+}
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { TodoItem },
+  name: 'App',
+  data(): DataTypes {
+    return {
+      todos: [],
+      inputValue: ''
+    }
   },
-});
+  methods: {
+    deleteTodo(todo: string) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    },
+    onSubmit() {
+      if (this.todos.indexOf(this.inputValue) >= 0)
+        return alert('Todo already exist')
+      this.todos.push(this.inputValue)
+      this.inputValue = ''
+    }
+  }
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <span>{{ todo }}</span>
+    <button @click="onDelete">Delete Todo</button>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TodoItem',
+  props: {
+    todo: String,
+    onDelete: Function
+  }
+})
+</script>
